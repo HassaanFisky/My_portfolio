058bc2796826eaf631af0c5df106dd4775f08d58
HassaanFisky <smokybrog@gmail.com>
2024-10-09 15:21:12 +0500

    Updated Portfolio

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Smooth scrolling for links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
 });
